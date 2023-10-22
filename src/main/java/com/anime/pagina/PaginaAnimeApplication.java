7340fb2e2b054a212f26e4900efede3b0c79c1ba
SparkleCow <cjelcazador200@outlook.com>
2023-10-22 00:08:52 -0500

    Inicio de proyecto de pagina web de anime. Primer Commit

---
+++ src/main/java/com/anime/pagina/PaginaAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.anime.pagina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaAnimeApplication.class, args);
+	}
+
+}
